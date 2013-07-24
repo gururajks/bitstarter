3945432af1438ded9407a18ed463775b1e07ff6e
Gururaj Sridhar <gururaj.genius@gmail.com>
2013-07-24 03:25:27 +0000

    changed port to 8080

--- web.js
+++ web.js
@@ -1,7 +1,8 @@
 var express = require('express');
 
 
-var app = express.createServer(express.logger());
+//var app = express.createServer(express.logger());
+var app = express();
 
 var fs = require('fs');
 var readFile = new Buffer(fs.readFileSync('index.html', 'utf8'));
@@ -10,7 +11,7 @@ app.get('/', function(request, response) {
 	response.send(readFile.toString('utf8'));
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
